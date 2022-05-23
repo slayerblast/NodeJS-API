53756b90cc60fbaf3617c8edd70420fe45387dde
root <root@LAPTOP-431L47VD.localdomain>
2022-05-23 14:06:56 +0200

    amelioration de la route /id

--- serveur.js
+++ serveur.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
 	res.send("ceci est la route utilisant la methode GET");
 })
 
-app.get('/id:arg1', (req, res)=>{
+app.get('/id/:id', (req, res)=>{
 	console.log(req.params)
 	res.send(req.params)
 
