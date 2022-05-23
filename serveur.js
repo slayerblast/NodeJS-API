b59a3720bb5b8bcc2f8b4c6efa758a2e4c79333a
root <root@LAPTOP-431L47VD.localdomain>
2022-05-23 14:03:36 +0200

    ajout de la route /id pouvant passer un parametre en argument

--- serveur.js
+++ serveur.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
 	res.send("ceci est la route utilisant la methode GET");
 })
 
+app.get('/id:arg1', (req, res)=>{
+	console.log(req.params)
+	res.send(req.params)
+
+})
 
 app.listen(3000);
 console.log('serveur lancé sur le port 3000')
