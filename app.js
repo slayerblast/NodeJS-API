d3fdbd56062112144d7281d22a99684a699dc3a7
root <root@LAPTOP-431L47VD.localdomain>
2022-05-23 16:14:25 +0200

    changement de nom: server.js => app.js + ajout de index.js => je lancerai le serveur ici et modification dans la route update => j'ai remplacé update par put

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const db = require("./db");
+
+app.use(express.json());
+
+const mapToObj = (m) => {
+    return Array.from(m).reduce((obj, [key, value]) => {
+      obj[key] = value;
+      return obj;
+    }, {});
+  };
+
+  
+app.post('/', (req, res) => {
+	const payload = req.body;
+  db.memoryDb.set(db['id']++, payload);
+  res.status(201).json(payload);
+})
+
+app.get('/', (req, res) => {
+	res.json(mapToObj(db.memoryDb));
+})
+
+app.get('/id/:id', (req, res)=>{
+    let id = parseInt(req.params.id)
+  res.json(db.memoryDb.get(id));
+
+})
+
+app.delete('/id/:id', function (req, res) {
+    let id = parseInt(req.params.id)
+    delete db.memoryDb[id];
+    res.send("tache effacé");
+  });
+
+app.put('/id/:id', function (req, res) {
+   
+    res.send("tache mis à jour");
+  });
+
+module.exports = app;
