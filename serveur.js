a25cd50671cd09f93a97ff1bd0aec4c2e4503bf5
root <root@LAPTOP-431L47VD.localdomain>
2022-05-23 13:49:30 +0200

    affichage d'un hello world avec la methode GET sur la route /

--- serveur.js
+++ serveur.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 
 
-app.post('/', (req, res) => {
-	console.log('test')
+app.get('/', (req, res) => {
+	res.send("Hello World");
+
 })
 
 app.listen(3000);
+console.log('serveur lancé sur le port 3000')
